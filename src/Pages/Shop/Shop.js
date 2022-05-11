d37adfd09a09eef3d1f6f7ebb1697b1543a43c62
arif99050 <arifulislam99050@gmail.com>
2022-05-11 09:54:42 +0600

    work with navabr

---
+++ src/Pages/Shop/Shop.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+            <div className='shop'>
+                <div>
+                    <h1>shop</h1>
+
+                </div>
+                <div>
+                    <h1>review</h1>
+
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
