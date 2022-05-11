b3b2030a74fe1746b665f8eb33d50eb3df1be111
arif99050 <arifulislam99050@gmail.com>
2022-05-11 15:12:37 +0600

    style on products

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import './Products.css'
 
 const Products = ({product}) => {
     const {name,img,seller,price,ratings}= product;
     return (
-        <div style={{border:"2px solid black"}}>
-            <img className='w-100' style={{height:"286px"}} src={img} alt="" />
-            <h1>{name}</h1>
-            <h3> Price :{price}</h3>
+        <div className='product shadow-lg p-3 mb-5 bg-white rounded ms-4' style={{height:"580px" , width:"350px"}}>
+            <img className='w-100 p-3' style={{height:"286px"}} src={img} alt="" />
+            <div className='p-3'>
+            <h4>{name}</h4>
+            <h6> Price :{price}</h6>
             <p>Seller : {seller}</p>
             <p>Ratings {ratings}</p>
+            <button className='btn btn-primary w-50 mx-auto' style={{MarginLeft:"10px"}}>Add to cart</button>
+
+            </div>
+            
+            
             
         </div>
     );
