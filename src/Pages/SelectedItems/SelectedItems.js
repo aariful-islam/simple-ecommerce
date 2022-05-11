19f891ac81e2963e43828732836c91eef55aaa3c
arif99050 <arifulislam99050@gmail.com>
2022-05-11 22:45:31 +0600

    cound total price of selected cart

--- src/Pages/SelectedItems/SelectedItems.js
+++ src/Pages/SelectedItems/SelectedItems.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './SelectedItems.css'
 
 const SelectedItems = ({cart}) => {
-    const {name}=cart;
+    let total=0;
+    for(const prod of cart){
+        total = total + prod.price;
+    }
     return (
-        <div>
+        <div className='p-3'>
+           <h5>Total Price:{total}</h5>
+           <h5>Total Shipping charge:</h5>
+           <h5>Tax:</h5>
+           <h2>Grand Total:</h2>
+
             
         </div>
     );
--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,54 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Products from '../Products/Products';
-import SelectedItems from '../SelectedItems/SelectedItems';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import Products from "../Products/Products";
+import SelectedItems from "../SelectedItems/SelectedItems";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products ,setProducts]= useState([])
-    const [carts,setCarts] = useState([]);
-    useEffect( ()=>{
-        fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
-    const handle=(products)=>{
-        const newCart=[...carts,products]
-        setCarts(newCart);
-        
-    }
-    return (
-        <div className='mt-4'>
-             <h1 className='text-center '>All products</h1>
-            <div className='shop'>
-               
-                <div className='products mt-3'>
-                {
-                    products.map(product => <Products
-                    product={product}
-                    key={product.id}
-                    handle={handle}
-                    
-                    ></Products>)
-                }
-
-                </div>
-                <div className='bg-primary text-white'>
-                    <h1 className='text-center'>Order Summery</h1>
-                     {
-                         carts.map(cart=> <SelectedItems 
-                         key={cart.key}
-                         cart={cart}
-                        
-                         
-
-                         ></SelectedItems>)
-                     }
-                     <h5 className='p-3'>total item : {carts.length}</h5>
-                </div>
-            </div>
-            
+  const [products, setProducts] = useState([]);
+  const [carts, setCarts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const handle = (products) => {
+    const newCart = [...carts, products];
+    setCarts(newCart);
+  };
+  return (
+    <div>
+      
+      <div className="shop">
+        <div className="products mt-3">
+          {products.map((product) => (
+            <Products
+              product={product}
+              key={product.id}
+              handle={handle}
+            ></Products>
+          ))}
         </div>
-    );
+        <div className="bg-primary text-white cart" style={{height:"700px"}}>
+          <h1 className="text-center">Order Summery</h1>
+          
+            <SelectedItems 
+            key={carts.key} 
+            cart={carts}
+            ></SelectedItems>
+         
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
