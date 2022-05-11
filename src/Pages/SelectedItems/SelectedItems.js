72b765e7f2da0a7a3ccf68e1512262b6f7d2270b
arif99050 <arifulislam99050@gmail.com>
2022-05-11 22:02:01 +0600

    work with selected product

---
+++ src/Pages/SelectedItems/SelectedItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SelectedItems = ({cart}) => {
+    const {name}=cart;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SelectedItems;
--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,30 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Products from '../Products/Products';
+import SelectedItems from '../SelectedItems/SelectedItems';
 import './Shop.css'
 
 const Shop = () => {
     const [products ,setProducts]= useState([])
+    const [carts,setCarts] = useState([]);
     useEffect( ()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    const handle=(products)=>{
+        const newCart=[...carts,products]
+        setCarts(newCart);
+        
+    }
     return (
-        <div>
+        <div className='mt-4'>
+             <h1 className='text-center '>All products</h1>
             <div className='shop'>
-                <div className='products'>
+               
+                <div className='products mt-3'>
                 {
                     products.map(product => <Products
                     product={product}
                     key={product.id}
+                    handle={handle}
                     
                     ></Products>)
                 }
 
                 </div>
-                <div>
-                    <h1>review</h1>
+                <div className='bg-primary text-white'>
+                    <h1 className='text-center'>Order Summery</h1>
+                     {
+                         carts.map(cart=> <SelectedItems 
+                         key={cart.key}
+                         cart={cart}
+                        
+                         
 
+                         ></SelectedItems>)
+                     }
+                     <h5 className='p-3'>total item : {carts.length}</h5>
                 </div>
             </div>
             
