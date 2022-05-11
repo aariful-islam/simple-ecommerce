da8871f396e1607205816dacd983961d20421eb4
arif99050 <arifulislam99050@gmail.com>
2022-05-11 22:57:11 +0600

    calculate grand total

--- src/Pages/SelectedItems/SelectedItems.js
+++ src/Pages/SelectedItems/SelectedItems.js
@@ -3,15 +3,21 @@ import './SelectedItems.css'
 
 const SelectedItems = ({cart}) => {
     let total=0;
+    let shipping =0;
     for(const prod of cart){
         total = total + prod.price;
+        shipping =shipping +prod.shipping;
     }
+    let tax=(total *0.1).toFixed(2);
+    let grandTotal =total+shipping+tax;
+    
     return (
         <div className='p-3'>
-           <h5>Total Price:{total}</h5>
-           <h5>Total Shipping charge:</h5>
-           <h5>Tax:</h5>
-           <h2>Grand Total:</h2>
+            <h5>Total Item :{cart.length}</h5>
+           <h5>Total Price:${total}</h5>
+           <h5>Total Shipping charge:${shipping}</h5>
+           <h5>Tax:${tax}</h5>
+           <h4>Grand Total:{grandTotal}</h4>
 
             
         </div>
