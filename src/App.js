b9d6165a0ff8a96cdef7936d76723c31062ab573
arif99050 <arifulislam99050@gmail.com>
2022-05-15 11:05:58 +0600

    work with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Header/Header';
 import Home from './Pages/Home/Home'
+import Orders from './Pages/Orders/Orders';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/order' element={<Orders></Orders>}></Route>
+      </Routes>
       
     </div>
   );
--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">Home</Navbar.Brand>
+  <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Order</Nav.Link>
+      <Nav.Link as={Link} to="/order">Order</Nav.Link>
       <Nav.Link href="#order">Order Review</Nav.Link>
      
     </Nav>
